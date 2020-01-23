663cdf6e08865296799d2c01a6645fe276b2b9e7
Andrew Baird <baird.andrew4@gmail.com>
2020-01-23 13:54:25 -0600

    Added three more Add tests that will throw an exception if index is out of range, check if the same value was added multiple times, check if the capacity increased if more than the current capacity was being added

--- CustomClassList/MyList.cs
+++ CustomClassList/MyList.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace CustomClassList
 {
-    class CustomList
+    public class MyList<T>
     {
+
     }
 }
--- CustomListTest/AddTest.cs
+++ CustomListTest/AddTest.cs
@@ -11,44 +11,127 @@ namespace CustomListTest
         public void Add_AddValueToList_CheckZeroIndex()
         {
             //Arrange
-            CustomList<int> myList = new CustomList<int>();
+            MyList<int> myList = new MyList<int>();
             int expected = 2;
+            int actual;
 
             //Act
             myList.Add(2);
+            actual = myList[0];
 
             //Assert
-            Assert.AreEqual(expected, myList[0]);
+            Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         public void Add_CheckCountAfterAdding_CountIs1()
         {
             //Arrange
-            CustomList<int> myList = new CustomList<int>();
+            MyList<int> myList = new MyList<int>();
             int initial= 2;
             int expected = 1;
+            int actual;
 
             //Act
             myList.Add(initial);
-
+            actual = myList.Count;
             //Assert
-            Assert.AreEqual(expected, myList.Count);
+            Assert.AreEqual(expected, actual);
         }
         [TestMethod]
         public void Add_CheckSecondValueAdd_ValueAtIndex1()
         {
             //Arrange
-            CustomList<int> myList = new CustomList<int>();
+            MyList<int> myList = new MyList<int>();
             int value = 2;
             int secondValue = 15;
+            int actual;
 
             //Act
             myList.Add(value);
             myList.Add(secondValue);
+            actual = myList[1]; 
 
             //Assert
-            Assert.AreEqual(secondValue, myList[1]);
+            Assert.AreEqual(secondValue, actual);
+        }
+        [TestMethod]
+        public void Add_CheckMultipleValuesAdd_ValueAtIndex2()
+        {
+            //Arrange
+            MyList<string> myList = new MyList<string>();
+            string value0 = "word";
+            string value1 = "work";
+            string value2 = "powerpoint";
+            string value3 = "excel";
+            string actual;
+
+            //Act
+            myList.Add(value0); 
+            myList.Add(value1);
+            myList.Add(value2);
+            myList.Add(value3);
+            actual = myList[2];
+
+            //Assert
+            Assert.AreEqual(value2, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Add_CheckIfListIsOutOfIndex_DisplayOutOfIndex()
+        {
+            // Arrange
+            MyList<int> myList = new MyList<int>();
+            int initial = 2;
+            
+            //Act
+            myList.Add();
+
+        }
+
+        [TestMethod]
+        public void Add_SameValueAddedMultipleTimes_ValueWasAddedAtLastIndex()
+        {
+            //Arrange
+            MyList<string> myList = new MyList<string>();
+            string value0 = "word";           
+
+            //Act
+            myList.Add(value0);
+            myList.Add(value0);
+            myList.Add(value0);
+            myList.Add(value0);
+            myList.Add(value0);
+            actual = myList[4];
+
+            //Act
+            Assert.AreEqual(value0, actual);
+        }
+
+        [TestMethod]
+        public void Add_MultipleValuesAdded_CapacityIncreases()
+        {
+            //Arrange
+            MyList<string> myList = new MyList<string>();
+            string value0 = "word";
+            string value1 = "work";
+            string value2 = "powerpoint";
+            string value3 = "excel";
+            string value4 = "chrome";
+            int actual;
+            int expected = 8;
+
+            //Act
+            myList.Add(value0);
+            myList.Add(value1);
+            myList.Add(value2);
+            myList.Add(value3);
+            myList.Add(value4);
+            actual = myList.Capacity;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
