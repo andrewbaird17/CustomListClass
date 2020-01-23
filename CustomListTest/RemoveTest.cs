db17d005b6ecdd50eca965551eba131ccf8f182d
Andrew Baird <baird.andrew4@gmail.com>
2020-01-23 15:24:23 -0600

    Added three remove tests in RemoveTest clas to (1) check if a zero is removed from an integer list the count stays 1, (2) check if the second value in a list of count 4 stays that value if the third item is remvoed, and (3) check to make sure the list count stays at three if a value not in the list is attempted to be removed.

--- CustomListTest/RemoveTest.cs
+++ CustomListTest/RemoveTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomClassList;
 
 namespace CustomListTest
 {
@@ -7,8 +8,67 @@ namespace CustomListTest
     public class RemoveTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Remove_RemoveOnlyItemValue0_CapacityStays4()
         {
+            //Arrange
+            MyList<int> myList = new MyList<int>();
+            int value = 5;
+            int valueRemove = 0;
+            int actual;
+            int expected = 4;
+
+            //Act
+            myList.Add(value);
+            myList.Remove(valueRemove);
+            actual = myList.Capacity;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Remove_FirstItemOutOfTwo_SecondItemMovesToIndex0()
+        {
+            //Arrange
+            MyList<int> myList = new MyList<int>();
+            int value1 = 5;
+            int value2 = 10;
+            int actual;
+            int expected = 10;
+
+            //Act
+            myList.Add(value1);
+            myList.Add(value2);
+            myList.Remove(value1);
+            actual = myList[0];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Remove_LastValue()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+
+        }
+
+        [TestMethod]
+        public void Remove_RemoveValueNotInList_CountStaysTheSame()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+            
         }
     }
 }
