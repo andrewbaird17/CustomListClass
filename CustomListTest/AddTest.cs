423dd91734b56dde6679b5aa3d2eac54e37bc6a8
Andrew Baird <baird.andrew4@gmail.com>
2020-01-23 12:01:42 -0600

    Made three tests of the Add method to Check if first value was added, check the count of the list after adding first value, check if a second value was added correctly

---
+++ CustomListTest/AddTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomClassList;
+
+namespace CustomListTest
+{
+    [TestClass]
+    public class AddTest
+    {
+        [TestMethod]
+        public void Add_AddValueToList_CheckZeroIndex()
+        {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int expected = 2;
+
+            //Act
+            myList.Add(2);
+
+            //Assert
+            Assert.AreEqual(expected, myList[0]);
+        }
+
+        [TestMethod]
+        public void Add_CheckCountAfterAdding_CountIs1()
+        {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int initial= 2;
+            int expected = 1;
+
+            //Act
+            myList.Add(initial);
+
+            //Assert
+            Assert.AreEqual(expected, myList.Count);
+        }
+        [TestMethod]
+        public void Add_CheckSecondValueAdd_ValueAtIndex1()
+        {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int value = 2;
+            int secondValue = 15;
+
+            //Act
+            myList.Add(value);
+            myList.Add(secondValue);
+
+            //Assert
+            Assert.AreEqual(secondValue, myList[1]);
+        }
+    }
+}
